04296678d602b6e277fdcc3617c299d7b4c3a429
H.M.E <1316126217@qq.com>
2022-02-07 21:42:34 +0800

    add  ColumnList

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,9 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
+  'extends': [
     'plugin:vue/vue3-essential',
-    '@vue/standard',
+    'eslint:recommended',
     '@vue/typescript/recommended'
   ],
   parserOptions: {
@@ -13,6 +13,7 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "@typescript-eslint/explicit-module-boundary-types": "off",
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
 }
