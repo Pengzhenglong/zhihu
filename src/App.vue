bea48d7d35e4230b93902a5aff5c76b95bc132b7
H.M.E <1316126217@qq.com>
2022-02-10 13:54:06 +0800

    添加表单验证

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,34 @@
   <div class="container">
     <global-header :user="currentUser"></global-header>
     <column-list :list="list"></column-list>
+    <form action="">
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
+        <input
+          type="text"
+          v-model="emailRef.val"
+          @blur="validateEmail"
+          class="form-control"
+          id="exampleInputEmail1"
+        />
+        <div class="form-text" v-if="emailRef.error">
+          {{ emailRef.message }}
+        </div>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">密码</label>
+        <input
+          type="password"
+          class="form-control"
+          id="exampleInputPassword1"
+        />
+      </div>
+    </form>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, reactive } from "vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ColumnList, { ColumnProps } from "./components/ColumnList.vue";
 import GlobalHeader, { UserProps } from "./components/GlobalHeader.vue";
@@ -48,9 +71,27 @@ export default defineComponent({
   name: "App",
   components: { ColumnList, GlobalHeader },
   setup() {
+    const emailRef = reactive({
+      val: "",
+      error: false,
+      message: "",
+    });
+    const emailReg =
+      /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    const validateEmail = () => {
+      if (emailRef.val.trim() === "") {
+        emailRef.error = true;
+        emailRef.message = "can  not  be  empty";
+      } else if (!emailReg.test(emailRef.val)) {
+        emailRef.error = true;
+        emailRef.message = "should  be  valid  meaage";
+      }
+    };
     return {
       list: testData,
       currentUser,
+      emailRef,
+      validateEmail,
     };
   },
 });
