897121f5a661c17a9c7a5509d84d99bfa471ecde
H.M.E <1316126217@qq.com>
2022-02-15 16:18:24 +0800

    添加 ValidateForm组件

--- src/App.vue
+++ src/App.vue
@@ -2,49 +2,48 @@
   <div class="container">
     <global-header :user="currentUser"></global-header>
     <column-list :list="list"></column-list>
-    <form action="">
+    <validate-form @form-submit="onFormSubmit">
       <div class="mb-3">
         <label class="form-label">邮箱地址</label>
         <validate-input
           :rules="emailRules"
           v-model="emailVal"
           placeholder="请输入邮箱地址"
-          type="password"
+          type="text"
         ></validate-input>
         {{ emailVal }}
       </div>
-      <!-- <div class="mb-3">
+      <div class="mb-3">
         <label class="form-label">密码</label>
         <validate-input
-          :rules="emailRules"
-          v-model="emailVal"
+          :rules="passwordRules"
+          v-model="passwordVal"
           placeholder="请输入密码"
-          type="text"
-        ></validate-input>
-    
-      </div> -->
-      <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
-        <input
-          type="text"
-          v-model="emailRef.val"
-          @blur="validateEmail"
-          class="form-control"
-          id="exampleInputEmail1"
-        />
-        <div class="form-text" v-if="emailRef.error">
-          {{ emailRef.message }}
-        </div>
-      </div>
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">密码</label>
-        <input
           type="password"
-          class="form-control"
-          id="exampleInputPassword1"
-        />
+        ></validate-input>
       </div>
-    </form>
+      <template #submit>
+        <span class="btn btn-danger">Submit</span>
+      </template>
+    </validate-form>
+    <!-- 封装组件前 -->
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
+      <input
+        type="text"
+        v-model="emailRef.val"
+        @blur="validateEmail"
+        class="form-control"
+        id="exampleInputEmail1"
+      />
+      <div class="form-text" v-if="emailRef.error">
+        {{ emailRef.message }}
+      </div>
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputPassword1" class="form-label">密码</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" />
+    </div>
   </div>
 </template>
 
@@ -54,6 +53,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import ColumnList, { ColumnProps } from "./components/ColumnList.vue";
 import GlobalHeader, { UserProps } from "./components/GlobalHeader.vue";
 import ValidateInput, { RulesProp } from "./components/ValidateInput.vue";
+import ValidateForm from "./components/ValidateForm.vue";
 const currentUser: UserProps = {
   isLogin: true,
   name: "DragonPeng",
@@ -91,13 +91,20 @@ const testData: ColumnProps[] = [
 ];
 export default defineComponent({
   name: "App",
-  components: { ColumnList, GlobalHeader, ValidateInput },
+  components: { ColumnList, GlobalHeader, ValidateInput, ValidateForm },
   setup() {
     const emailVal = ref("dragonPeng");
     const emailRules: RulesProp = [
       { type: "required", message: "电子邮箱地址不能为空" },
       { type: "email", message: "请输入正确的电子邮箱格式" },
     ];
+    const passwordVal = ref("");
+    const passwordRules: RulesProp = [
+      { type: "required", message: "密码不能为空" },
+    ];
+    const onFormSubmit = (result: boolean) => {
+      console.log("123", result);
+    };
     const emailRef = reactive({
       val: "",
       error: false,
@@ -119,8 +126,11 @@ export default defineComponent({
       currentUser,
       emailRef,
       validateEmail,
+      passwordVal,
+      passwordRules,
       emailRules,
       emailVal,
+      onFormSubmit,
     };
   },
 });
