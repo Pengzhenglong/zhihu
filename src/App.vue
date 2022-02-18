c9ed19ae0b4f6c79e6cfd9a1f5021094c5c80016
H.M.E <1316126217@qq.com>
2022-02-18 13:18:34 +0800

    添加footer

--- src/App.vue
+++ src/App.vue
@@ -45,6 +45,19 @@
       <label for="exampleInputPassword1" class="form-label">密码</label>
       <input type="password" class="form-control" id="exampleInputPassword1" />
     </div>
+
+    <!-- footer -->
+    <footer class="text-center py-4 text-secondary bg-light mt-6">
+      <small>
+        <ul class="list-inline mb-0">
+          <li class="list-inline-item">© 2020 者也专栏</li>
+          <li class="list-inline-item">课程</li>
+          <li class="list-inline-item">文档</li>
+          <li class="list-inline-item">联系</li>
+          <li class="list-inline-item">更多</li>
+        </ul>
+      </small>
+    </footer>
   </div>
 </template>
 
