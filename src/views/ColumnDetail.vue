62ed5c8098883aba7333835951200666bb661998
H.M.E <1316126217@qq.com>
2022-02-25 10:27:37 +0800

    添加路由

---
+++ src/views/ColumnDetail.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <pre>
+       {{ route }}  
+    </pre>
+  </div>
+</template>
+
+<script  lang="ts">
+import { defineComponent } from "vue";
+import { useRouter } from "vue-router";
+export default defineComponent({
+  name: "ColumnDetail",
+  setup() {
+    const route = useRouter();
+    return {
+      route,
+    };
+  },
+});
+</script>
+
+<style>
+</style>
